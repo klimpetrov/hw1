8c80131b8e817dd3f28cc79516bf663199d459f3
Klim Petrov <klim.petrov@hhu.com>
2015-11-04 20:34:13 +0100

    Klim P. HWKlim P. HWKlim P. HWKlim P. HW1111

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int N=0;
+	cout <<"N"<< endl;
+	cin >> N;
+	if(N==0){
+	cout <<"Fibonacci-Zahl" <<" "<<"ist"<<" "<<0<<endl;
+	}
+	if(N==1){
+	cout <<"Fibonacci-Zahl" <<" "<<"ist"<<" "<<1<<endl;
+	}
+	if(N>1){
+		int x=1;
+		int y=0;
+		int f;
+		for(int i=1;i<N;i++){
+			f=x+y;
+			y=x;
+			x=f;
+		}
+	cout <<"Fibonacci Zahl" <<" "<<"ist"<<" "<<x<<endl;
+	}
+return 0;
 
+}	
